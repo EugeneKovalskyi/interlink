f552f4365fd4675c80c8a556dbafc77074264124
Yevhen Kovalskyi <yevhenko@tahometer.com>
2022-02-03 12:41:23 +0200

    aggregation part 1: ready

--- week2/routes/lists.js
+++ week2/routes/lists.js
@@ -5,29 +5,3 @@ router.get('/', controller.find)
 router.get('/:listId', controller.findById)
 
 module.exports = router
-
-// const http = require('http')
-// const server = http.createServer(callback)
-
-// server.listen(3000, () => console.log('Server started...'))
-
-// function callback(req, res) {
-//   if (req.method === 'GET') {
-//     if (req.url === '/tasks') {
-//       res.writeHeader(200, {
-//         'Content-Type': 'application/json',
-//       })
-
-//       res.end(req.body)
-//     }
-//   } else if (req.method === 'POST') {
-//     const data = []
-
-//     req
-//       .on('data', (chunk) => data.push(chunk))
-//       .on('end', createTask(data.join('').toString()))
-//   } else {
-//     res.writeHead(404, 'Not Found')
-//     res.end()
-//   }
-// }
