bccc5fcefd77ee8f56894cf7aae8420ed8acc94c
Yevhen Kovalskyi <yevhenko@tahometer.com>
2022-02-22 10:52:51 +0200

    add styles for overdue and done tasks

--- week5/react-todolist/src/components/Task.js
+++ week5/react-todolist/src/components/Task.js
@@ -1,7 +1,28 @@
 import React from 'react'
 import styles from '../style/Task.module.css'
 
-export default function Task({ task, setTaskDone }) {
+export default function Task({ task, setTask }) {
+  const titleClasses = []
+  const dateClasses = [styles.date]
+  const descriptionClasses = [styles.description]
+
+  function isOverdue() {
+    const currentTime = new Date().getTime()
+    const dueTime = new Date(task.due_date).getTime()
+
+    return currentTime > dueTime ? true : false
+  }
+
+  if (task.done) {
+    titleClasses.push(styles.done)
+    dateClasses.push(styles.done)
+    descriptionClasses.push(styles.done)
+  }
+
+  if (isOverdue()) {
+    dateClasses.push(styles.overdue)
+  }
+
   return (
     <div className={styles.task}>
       <button className={styles.delete}>X</button>
@@ -11,12 +32,14 @@ export default function Task({ task, setTaskDone }) {
           id={`task-${task.id}`}
           type='checkbox'
           checked={task.done}
-          onChange={() => setTaskDone(task.id)}
+          onChange={() => setTask(task.id)}
         />
-        <span>{task.title}</span>
+        <span className={titleClasses.join(' ')}>{task.title}</span>
       </label>
-      <span className={styles.date}>Due date: {task.due_date}</span>
-      <p className={styles.description}>Description: {task.description}</p>
+      <span className={dateClasses.join(' ')}>Due date: {task.due_date}</span>
+      <p className={descriptionClasses.join(' ')}>
+        Description: {task.description}
+      </p>
     </div>
   )
 }
